f3502e90dbe2f497da3ea3b911890e42ce3762a0
Beth Fraser <bethanyfraser@live.co.uk>
2016-05-11 16:23:50 +0100

    trying to fix heroku

--- server.js
+++ server.js
@@ -2,15 +2,21 @@ var express = require('express');
 var app = express();
 var path = require('path')
 
+app.set('port', (process.env.PORT || 5000));
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.use(express.static('public'));
 
-var server = app.listen(5000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+// var server = app.listen(5000, function () {
+//   var host = server.address().address;
+//   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+//   console.log('Example app listening at http://%s:%s', host, port);
+// });
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
